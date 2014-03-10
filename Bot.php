5ec114bd74e7cadde67ce37693a4569191192a30
fubuki <fubuki@users.noreply.github.com>
2014-03-10 23:01:35 +0800

    建立一個基本Bot原型。

---
+++ Bot.php
@@ -0,0 +1,46 @@
+<?php
+
+Class Bot 
+{
+	private $agent;
+
+	public function __construct()
+ 	{
+ 
+  	}
+
+  	/**
+  	 * 計算從一個網頁到另外一個網頁路徑
+  	 * @param  string $start 
+  	 * @param  string $end
+  	 * @return [type]
+  	 */
+	public function urlWalkPath($start, $end)
+	{
+
+
+	}
+
+	/**
+	 * 取得一個網頁裡所有連結
+	 * @param  [type] $url
+	 * @return [type]
+	 */
+	public function getUrlContent($url)
+	{
+
+	}
+
+	/**
+	 * 找出連結跟其他連結之間相關性最大的部分
+	 * @param  string $url
+	 * @param  array $entity
+	 * @return [type]
+	 */
+	public function compareUrlSimilar($url, $entity)
+	{
+
+
+	}
+
+}
